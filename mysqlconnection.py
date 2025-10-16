d57274debf1f4299e462ba697bcf48d8dfe7a487
sujith <sujith@gmail.com>
2025-10-16 17:52:54 +0530

    changed host name

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # Establish connection to the database
 mydb = mysql.connector.connect(
-  host="3306",
+  host="mydb",
   user= os.getenv("youruser"),
   password= os.getenv("yourpassword"),
   database="yourdb"
