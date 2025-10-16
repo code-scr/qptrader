c7cd27e09343ff977a17b1c34350dfd96f920b63
sujith <sujith@gmail.com>
2025-10-16 11:35:08 +0530

    changed compose file

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -6,10 +6,10 @@ from datetime import datetime
 
 # Establish connection to the database
 mydb = mysql.connector.connect(
-    host=os.environ.get("DB_HOST", "localhost"),
-    user=os.environ.get("QP_USER_NAME"),
-    password=os.environ.get("QP_PASSWORD"),
-    database="mydatabase"
+    host=os.environ.get("MYSQL_HOST", "localhost"),
+    user=os.environ.get("MYSQL_USER", "root"),
+    password=os.environ.get("MYSQL_PASSWORD", ""),
+    database=os.environ.get("MYSQL_DATABASE", "")
 )
 
 def updatedb(data):
