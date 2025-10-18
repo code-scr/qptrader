95dd936ec7b477d9a23bdba7151868f03a6b1cad
sujith <sujith@gmail.com>
2025-10-18 11:03:50 +0530

    small change in sql

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -11,7 +11,7 @@ mydb = mysql.connector.connect(
     host=os.getenv("DB_HOST", "localhost"),
     user=os.getenv("DB_USER", "qptrader_user"),
     password=os.getenv("DB_PASSWORD", "qptrader_password"),
-    database=os.getenv("DB_NAME", "qptrader-db")
+    database=os.getenv("DB_NAME", "qptrader_db")
 )
 
 
