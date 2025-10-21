92d702a649f569017fce1b1ce6cb2c195a48fd40
sujith <sujith@gmail.com>
2025-10-21 19:47:01 +0530

    removed some js what i added

--- static/main.js
+++ static/main.js
@@ -1,19 +1,12 @@
 // Realtime Database
-const pin = document.getElementById("username");
-const date = document.getElementById("password");
+const pin = document.getElementById("pin");
+const date = document.getElementById("date");
 const btnSubmit = document.getElementById("submit");
 
 const database = firebase.database();
 const rootRef = database.ref('/users/');
 
 btnSubmit.addEventListener('click', (e)=> {
-  e.preventDefault(); // Prevent the page from reloading
-
-  // Input validation
-  if (pin.value === "" || date.value === "") {
-    alert("Please fill all fields");
-    return; // Stop execution if fields are empty
-  }
   const autoId = rootRef.push().key
   rootRef.child(autoId).set({
     pin: pin.value,
