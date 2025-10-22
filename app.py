682b88f6a109228989425d88de849f57c2daea0c
sujith <sujith@gmail.com>
2025-10-23 00:20:25 +0530

    added keys

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ app.config["DEBUG"] = True
 app.secret_key = 'fnyhwrbc1fyfulg3opt6pkj25nagxphi'
 
 # Replace with your actual API key and access token
-api_key = "oz8srgm6p4nfg2ee"
+api_key = os.getenv("API_KEY")
 access_token = os.getenv("ACCESS_TOKEN")
 BASE_URL = 'https://kite.zerodha.com/'
 position_details = []  # Replace with your actual symbols      
